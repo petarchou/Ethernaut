b3ce232a35822104cce23f9471bada0f4d7c7904
Petar Petrov <ppetrovbusiness@gmail.com>
2023-05-29 14:12:49 +0300

    Add level 15 attack

---
+++ test/15_Preservation.t.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import "src/15_Preservation/Level.sol";
+import "src/15_Preservation/Attacker.sol";
+
+contract PreservationTest is Test {
+
+    LibraryContract lib1 = new LibraryContract();
+    LibraryContract lib2 = new LibraryContract();
+
+    Preservation target = new Preservation(address(lib1), address(lib2));
+    Attacker attacker = new Attacker(address(target));
+
+    function test_attack() public {
+        // address sender = makeAddr("sender");
+        // vm.prank(sender);
+        attacker.attack();
+        address owner = target.owner();
+        assertEq(msg.sender, owner);
+    }
+   
+    
+}
